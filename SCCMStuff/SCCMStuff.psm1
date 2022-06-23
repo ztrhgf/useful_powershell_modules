5bcc826189a2d3e0bb825d83ddd74ae7fa5ceb70
ondrejs4 <ondrejs4@kentico.com>
2022-06-23 22:31:28 +0200

    Get-CMAutopilotHash

--- SCCMStuff/SCCMStuff.psm1
+++ SCCMStuff/SCCMStuff.psm1
@@ -388,6 +388,71 @@ function Get-CMApplicationOGV {
     sort LocalizedDisplayName | ogv -OutputMode Multiple -Title $title | select -exp LocalizedDisplayName
 }
 
+function Get-CMAutopilotHash {
+    <#
+    .SYNOPSIS
+    Function for getting Autopilot hash from SCCM database.
+
+    .DESCRIPTION
+    Function for getting Autopilot hash from SCCM database.
+    Hash is by default gathered during Hardware inventory cycle.
+
+    .PARAMETER computerName
+    Name of the computer you want to get the hash for.
+    If omitted, hashes for all computers will be returned.
+
+    .PARAMETER SCCMServer
+    Name of the SCCM server.
+
+    .PARAMETER SCCMSiteCode
+    SCCM site code
+
+    .EXAMPLE
+    Get-CMAutopilotHash -computerName "PC-01" -SCCMServer "CMServer" -SCCMSiteCode "XXX"
+
+    Get Autopilot hash for PC-01 computer from SCCM server.
+
+    .EXAMPLE
+    Get-CMAutopilotHash -SCCMServer "CMServer" -SCCMSiteCode "XXX"
+
+    Get Autopilot hash for all computers from SCCM server.
+
+    .NOTES
+    Requires function Invoke-SQL and permission to read data from SCCM database.
+    #>
+
+    [CmdletBinding()]
+    param (
+        [string[]] $computerName,
+
+        [ValidateNotNullOrEmpty()]
+        $SCCMServer = $_SCCMServer,
+
+        [ValidateNotNullOrEmpty()]
+        $SCCMSiteCode = $_SCCMSiteCode
+    )
+
+    $sql = @'
+SELECT
+distinct(bios.SerialNumber0) as "SerialNumber",
+System.Name0 as "Hostname",
+User_Name0 as "Owner",
+(osinfo.SerialNumber0) as "WindowsProductID",
+mdminfo.DeviceHardwareData0 as "HardwareHash"
+FROM v_R_System System
+Inner Join v_GS_PC_BIOS bios on System.ResourceID=bios.ResourceID
+Inner Join v_GS_OPERATING_SYSTEM osinfo on System.ResourceID=osinfo.ResourceID
+Inner Join v_GS_MDM_DEVDETAIL_EXT01 mdminfo on System.ResourceID=mdminfo.ResourceID
+'@
+
+    if ($computerName) {
+        $sql = "$sql WHERE System.Name0 IN ($(($computerName | % {"'"+$_+"'"}) -join ", "))"
+        Write-Verbose $sql
+    }
+
+    Invoke-SQL -dataSource $SCCMServer -database "CM_$SCCMSiteCode" -sqlCommand $sql
+}
+
 function Get-CMCollectionComplianceStatus {
     [CmdletBinding()]
     param (
@@ -2826,6 +2891,6 @@ function Update-CMClientPolicy {
     }
 }
 
-Export-ModuleMember -function Add-CMDeviceToCollection, Clear-CMClientCache, Connect-SCCM, Get-CMAppDeploymentStatus, Get-CMApplicationOGV, Get-CMCollectionComplianceStatus, Get-CMCollectionOGV, Get-CMComputerCollection, Get-CMComputerComplianceStatus, Get-CMDeploymentStatus, Get-CMLog, Invoke-CMAdminServiceQuery, Invoke-CMAppInstall, Invoke-CMClientReinstall, Invoke-CMComplianceEvaluation, New-CMAppDeployment, New-CMAppPhasedDeploment, New-CMDevice, Refresh-CMCollection, Set-CMDeviceDJoinBlobVariable, Set-CMDeviceSerialNumber, Update-CMAppSourceContent, Update-CMClientPolicy
+Export-ModuleMember -function Add-CMDeviceToCollection, Clear-CMClientCache, Connect-SCCM, Get-CMAppDeploymentStatus, Get-CMApplicationOGV, Get-CMAutopilotHash, Get-CMCollectionComplianceStatus, Get-CMCollectionOGV, Get-CMComputerCollection, Get-CMComputerComplianceStatus, Get-CMDeploymentStatus, Get-CMLog, Invoke-CMAdminServiceQuery, Invoke-CMAppInstall, Invoke-CMClientReinstall, Invoke-CMComplianceEvaluation, New-CMAppDeployment, New-CMAppPhasedDeploment, New-CMDevice, Refresh-CMCollection, Set-CMDeviceDJoinBlobVariable, Set-CMDeviceSerialNumber, Update-CMAppSourceContent, Update-CMClientPolicy
 
 Export-ModuleMember -alias Invoke-CMAppDeployment, Invoke-CMAppPhasedDeployment, Invoke-CMClientPolicyUpdate, Set-CMDeviceServiceTag
---
+++ SCCMStuff_source/Get-CMAutopilotHash.ps1
@@ -0,0 +1,64 @@
+﻿function Get-CMAutopilotHash {
+    <#
+    .SYNOPSIS
+    Function for getting Autopilot hash from SCCM database.
+
+    .DESCRIPTION
+    Function for getting Autopilot hash from SCCM database.
+    Hash is by default gathered during Hardware inventory cycle.
+
+    .PARAMETER computerName
+    Name of the computer you want to get the hash for.
+    If omitted, hashes for all computers will be returned.
+
+    .PARAMETER SCCMServer
+    Name of the SCCM server.
+
+    .PARAMETER SCCMSiteCode
+    SCCM site code
+
+    .EXAMPLE
+    Get-CMAutopilotHash -computerName "PC-01" -SCCMServer "CMServer" -SCCMSiteCode "XXX"
+
+    Get Autopilot hash for PC-01 computer from SCCM server.
+
+    .EXAMPLE
+    Get-CMAutopilotHash -SCCMServer "CMServer" -SCCMSiteCode "XXX"
+
+    Get Autopilot hash for all computers from SCCM server.
+
+    .NOTES
+    Requires function Invoke-SQL and permission to read data from SCCM database.
+    #>
+
+    [CmdletBinding()]
+    param (
+        [string[]] $computerName,
+
+        [ValidateNotNullOrEmpty()]
+        $SCCMServer = $_SCCMServer,
+
+        [ValidateNotNullOrEmpty()]
+        $SCCMSiteCode = $_SCCMSiteCode
+    )
+
+    $sql = @'
+SELECT
+distinct(bios.SerialNumber0) as "SerialNumber",
+System.Name0 as "Hostname",
+User_Name0 as "Owner",
+(osinfo.SerialNumber0) as "WindowsProductID",
+mdminfo.DeviceHardwareData0 as "HardwareHash"
+FROM v_R_System System
+Inner Join v_GS_PC_BIOS bios on System.ResourceID=bios.ResourceID
+Inner Join v_GS_OPERATING_SYSTEM osinfo on System.ResourceID=osinfo.ResourceID
+Inner Join v_GS_MDM_DEVDETAIL_EXT01 mdminfo on System.ResourceID=mdminfo.ResourceID
+'@
+
+    if ($computerName) {
+        $sql = "$sql WHERE System.Name0 IN ($(($computerName | % {"'"+$_+"'"}) -join ", "))"
+        Write-Verbose $sql
+    }
+
+    Invoke-SQL -dataSource $SCCMServer -database "CM_$SCCMSiteCode" -sqlCommand $sql
+}

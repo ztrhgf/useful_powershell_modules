7d69aa625f6e4c0a5dd8b4a0bb47d5b23fbf3621
ondrejs4 <ondrejs4@kentico.com>
2022-07-03 21:32:16 +0200

    Start-AzureADSync

--- AzureADStuff/AzureADStuff.psm1
+++ AzureADStuff/AzureADStuff.psm1
@@ -4076,6 +4076,52 @@ function Remove-AzureADAppUserConsent {
     }
 }
 
-Export-ModuleMember -function Add-AzureADAppCertificate, Add-AzureADAppUserConsent, Add-AzureADGuest, Connect-AzAccount2, Connect-AzureAD2, Connect-PnPOnline2, Disable-AzureADGuest, Get-AzureADAccountOccurrence, Get-AzureADAppConsentRequest, Get-AzureADAppRegistration, Get-AzureADAppUsersAndGroups, Get-AzureADAppVerificationStatus, Get-AzureADAssessNotificationEmail, Get-AzureADEnterpriseApplication, Get-AzureAdGroupMemberRecursive, Get-AzureADManagedIdentity, Get-AzureADResource, Get-AzureADRoleAssignments, Get-AzureADServicePrincipalOverview, Get-AzureADSPPermissions, Get-AzureDevOpsOrganizationOverview, Get-SharepointSiteOwner, Invoke-GraphAPIRequest, New-AzureADMSIPConditionalAccessPolicy, New-AzureDevOpsAuthHeader, New-GraphAPIAuthHeader, Open-AzureADAdminConsentPage, Remove-AzureADAccountOccurrence, Remove-AzureADAppUserConsent
+function Start-AzureADSync {
+    <#
+        .SYNOPSIS
+        Invoke Azure AD sync cycle command (Start-ADSyncSyncCycle) on the server where 'Azure AD Connect' is installed.
 
-Export-ModuleMember -alias Get-AzureADIAMRoleAssignments, Get-AzureADPSPermissionGrants, Get-AzureADPSPermissions, Get-AzureADRBACRoleAssignments, Get-AzureADServiceAppRoleAssignment2, Get-AzureADServicePrincipal2, Get-AzureADServicePrincipalPermissions, Get-IntuneAuthHeader, New-AzureADGuest, New-IntuneAuthHeader, Remove-AzureADGuest
+        .DESCRIPTION
+        Invoke Azure AD sync cycle command (Start-ADSyncSyncCycle) on the server where 'Azure AD Connect' is installed.
+
+        .PARAMETER Type
+        Type of sync.
+
+        Initial (full) or just delta.
+
+        Delta is default.
+
+        .PARAMETER ADSynchServer
+        Name of the server where 'Azure AD Connect' is installed
+
+        .EXAMPLE
+        Start-AzureADSync -ADSynchServer ADSYNCSERVER
+        Invokes synchronization between on-premises AD and AzureAD on server ADSYNCSERVER by running command Start-ADSyncSyncCycle there.
+    #>
+
+    [Alias("Sync-ADtoAzure")]
+    [cmdletbinding()]
+    param (
+        [ValidateSet('delta', 'initial')]
+        [string] $type = 'delta',
+
+        [Parameter(Mandatory = $true)]
+        [string] $ADSynchServer
+    )
+
+    $ErrState = $false
+    do {
+        try {
+            Invoke-Command -ScriptBlock { Start-ADSyncSyncCycle -PolicyType $using:type } -ComputerName $ADSynchServer -ErrorAction Stop | Out-Null
+            $ErrState = $false
+        } catch {
+            $ErrState = $true
+            Write-Warning "Start-AzureADSync: Error in Sync:`n$_`nRetrying..."
+            Start-Sleep 5
+        }
+    } while ($ErrState -eq $true)
+}
+
+Export-ModuleMember -function Add-AzureADAppCertificate, Add-AzureADAppUserConsent, Add-AzureADGuest, Connect-AzAccount2, Connect-AzureAD2, Connect-PnPOnline2, Disable-AzureADGuest, Get-AzureADAccountOccurrence, Get-AzureADAppConsentRequest, Get-AzureADAppRegistration, Get-AzureADAppUsersAndGroups, Get-AzureADAppVerificationStatus, Get-AzureADAssessNotificationEmail, Get-AzureADEnterpriseApplication, Get-AzureAdGroupMemberRecursive, Get-AzureADManagedIdentity, Get-AzureADResource, Get-AzureADRoleAssignments, Get-AzureADServicePrincipalOverview, Get-AzureADSPPermissions, Get-AzureDevOpsOrganizationOverview, Get-SharepointSiteOwner, Invoke-GraphAPIRequest, New-AzureADMSIPConditionalAccessPolicy, New-AzureDevOpsAuthHeader, New-GraphAPIAuthHeader, Open-AzureADAdminConsentPage, Remove-AzureADAccountOccurrence, Remove-AzureADAppUserConsent, Start-AzureADSync
+
+Export-ModuleMember -alias Get-AzureADIAMRoleAssignments, Get-AzureADPSPermissionGrants, Get-AzureADPSPermissions, Get-AzureADRBACRoleAssignments, Get-AzureADServiceAppRoleAssignment2, Get-AzureADServicePrincipal2, Get-AzureADServicePrincipalPermissions, Get-IntuneAuthHeader, New-AzureADGuest, New-IntuneAuthHeader, Remove-AzureADGuest, Sync-ADtoAzure
---
+++ AzureADStuff_source/Start-AzureADSync.ps1
@@ -0,0 +1,45 @@
+function Start-AzureADSync {
+    <#
+        .SYNOPSIS
+        Invoke Azure AD sync cycle command (Start-ADSyncSyncCycle) on the server where 'Azure AD Connect' is installed.
+
+        .DESCRIPTION
+        Invoke Azure AD sync cycle command (Start-ADSyncSyncCycle) on the server where 'Azure AD Connect' is installed.
+
+        .PARAMETER Type
+        Type of sync.
+
+        Initial (full) or just delta.
+
+        Delta is default.
+
+        .PARAMETER ADSynchServer
+        Name of the server where 'Azure AD Connect' is installed
+
+        .EXAMPLE
+        Start-AzureADSync -ADSynchServer ADSYNCSERVER
+        Invokes synchronization between on-premises AD and AzureAD on server ADSYNCSERVER by running command Start-ADSyncSyncCycle there.
+    #>
+
+    [Alias("Sync-ADtoAzure")]
+    [cmdletbinding()]
+    param (
+        [ValidateSet('delta', 'initial')]
+        [string] $type = 'delta',
+
+        [Parameter(Mandatory = $true)]
+        [string] $ADSynchServer
+    )
+
+    $ErrState = $false
+    do {
+        try {
+            Invoke-Command -ScriptBlock { Start-ADSyncSyncCycle -PolicyType $using:type } -ComputerName $ADSynchServer -ErrorAction Stop | Out-Null
+            $ErrState = $false
+        } catch {
+            $ErrState = $true
+            Write-Warning "Start-AzureADSync: Error in Sync:`n$_`nRetrying..."
+            Start-Sleep 5
+        }
+    } while ($ErrState -eq $true)
+}

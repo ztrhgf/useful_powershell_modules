e84fb5f1e7535fc8d510cecd4ee6f9e0567f6c50
OndrejS4 <OndrejS4@kentico.com>
2022-08-24 16:05:37 +0200

    divide get and redeploy of win32apps to separate functions

--- IntuneStuff_source/Invoke-IntuneWin32AppRedeploy.ps1
+++ IntuneStuff_source/Invoke-IntuneWin32AppRedeploy.ps1
@@ -47,118 +47,14 @@
 
         [System.Management.Automation.PSCredential] $credential,
 
-        [string] $tenantId,
-
-        [switch] $excludeSystemApp
+        [string] $tenantId
     )
 
+    if (!(Get-Command Get-IntuneWin32App)) {
+        throw "Command Get-IntuneWin32App is missing"
+    }
+
     #region helper function
-    # function translates user Azure ID or SID to its display name
-    function _getTargetName {
-        param ([string] $id)
-
-        Write-Verbose "Translating account $id to its name (SID)"
-
-        if (!$id) {
-            Write-Verbose "Id was null"
-            return
-        } elseif ($id -eq 'device') {
-            # xml nodes contains 'device' instead of 'Device'
-            return 'Device'
-        }
-
-        $errPref = $ErrorActionPreference
-        $ErrorActionPreference = "Stop"
-        try {
-            if ($id -eq '00000000-0000-0000-0000-000000000000' -or $id -eq 'S-0-0-00-0000000000-0000000000-000000000-000') {
-                Write-Verbose "`t- Id belongs to device"
-                return 'Device'
-            } elseif ($id -match "^S-\d+-\d+-\d+") {
-                # it is local account
-                Write-Verbose "`t- Id is SID, trying to translate to local account name"
-                return ((New-Object System.Security.Principal.SecurityIdentifier($id)).Translate([System.Security.Principal.NTAccount])).Value
-            } else {
-                # it is AzureAD account
-                Write-Verbose "`t- Id belongs to AAD account"
-                if ($getDataFromIntune) {
-                    Write-Verbose "`t- Translating ID using Intune data"
-                    return ($intuneUser | ? id -EQ $id).userPrincipalName
-                } else {
-                    Write-Verbose "`t- Getting SID that belongs to AAD ID, by searching Intune logs"
-                    $userSID = Get-IntuneUserSID $id
-                    if ($userSID) {
-                        _getTargetName $userSID
-                    } else {
-                        return $id
-                    }
-                }
-            }
-        } catch {
-            Write-Warning "Unable to translate $id to account name ($_)"
-            $ErrorActionPreference = $errPref
-            return $id
-        }
-    }
-
-    # function translates user Azure ID to local SID, by getting such info from Intune log files
-    function Get-IntuneUserSID {
-        param (
-            [Parameter(Mandatory = $true)]
-            [string] $userId
-        )
-
-        $intuneLogList = Get-ChildItem -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs" -Filter "IntuneManagementExtension*.log" -File | sort LastWriteTime -Descending | select -ExpandProperty FullName
-
-        if (!$intuneLogList) {
-            Write-Error "Unable to find any Intune log files. Redeploy will probably not work as expected."
-            return
-        }
-
-        foreach ($intuneLog in $intuneLogList) {
-            # how content of the log can looks like
-            # [Win32App] ..................... Processing user session 1, userId: e5834928-0f19-492d-8a69-3fbc98fd84eb, userSID: S-1-5-21-2475586523-545188003-3344463812-8050 .....................
-            # [Win32App] EspPreparation starts for userId: e5834928-0f19-442d-8a69-3fbc98fd84eb userSID: S-1-5-21-2475586523-545182003-3344463812-8050
-
-            $userMatch = Select-String -Path $intuneLog -Pattern "(?:\[Win32App\] \.* Processing user session \d+, userId: $userId, userSID: (S-[0-9-]+) )|(?:\[Win32App\] EspPreparation starts for userId: $userId userSID: (S-[0-9-]+))" -List
-            if ($userMatch) {
-                # return user SID
-                return $userMatch.matches.groups[1].value
-            }
-        }
-
-        Write-Warning "Unable to find User '$userId' in any of the Intune log files. Unable to translate this AAD ID to local SID."
-    }
-
-    # function translates app Azure ID to name, by getting such info from Intune log files
-    function Get-IntuneWin32AppName {
-        param (
-            [Parameter(Mandatory = $true)]
-            [string] $appId
-        )
-
-        $intuneLogList = Get-ChildItem -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs" -Filter "IntuneManagementExtension*.log" -File | sort LastWriteTime -Descending | select -ExpandProperty FullName
-
-        if (!$intuneLogList) {
-            Write-Error "Unable to find any Intune log files. Redeploy will probably not work as expected."
-            return
-        }
-
-        foreach ($intuneLog in $intuneLogList) {
-            # how content of the log can looks like
-            # [Win32App] app result (id = e524e208-0758-4b38-a15a-60688778421c, name = BuiltinAdminPSProfile.ps1, version = 2) is different from cached one, save to cache.
-            # [Win32App] Processing app (id=e524e208-0758-4b38-a15a-60688778421c, name = BuiltinAdminPSProfile.ps1) with mode = DetectInstall	IntuneManagementExtension	26.07.2022 15:44:56	80 (0x0050)
-
-            $appMatch = Select-String -Path $intuneLog -Pattern "(?:\[Win32App\] ExecManager: processing targeted app \(name='([^,]+)', id='$appId'\))|(?:\[Win32App\] app result \(id = $appId, name = ([^,]+),)" -List
-            if ($appMatch) {
-                # return app name
-                return $appMatch.matches.groups[1].value
-            }
-        }
-
-        Write-Warning "Unable to find App '$appId' in any of the Intune log files. Unable to translate its ID to Name."
-        return "*unable to obtain from local logs*"
-    }
-
     # function gets app GRS hash from Intune log files
     function Get-Win32AppGRSHash {
         param (
@@ -187,140 +83,26 @@
 
         Write-Error "Unable to find App '$appId' GRS hash in any of the Intune log files. Redeploy will probably not work as expected"
     }
-
     # create helper functions text definition for usage in remote sessions
-    $allFunctionDefs = "function _getTargetName { ${function:_getTargetName} }; function Get-Win32AppGRSHash { ${function:Get-Win32AppGRSHash} }; function Get-IntuneWin32AppName { ${function:Get-IntuneWin32AppName} }; function Get-IntuneUserSID { ${function:Get-IntuneUserSID} }"
+    $allFunctionDefs = "function Get-Win32AppGRSHash { ${function:Get-Win32AppGRSHash} };"
     #endregion helper function
 
-    #region prepare
-    if ($getDataFromIntune) {
-        if (!(Get-Module 'Microsoft.Graph.Intune') -and !(Get-Module 'Microsoft.Graph.Intune' -ListAvailable)) {
-            throw "Module 'Microsoft.Graph.Intune' is required. To install it call: Install-Module 'Microsoft.Graph.Intune' -Scope CurrentUser"
-        }
+    #region get deployed Win32Apps
+    $param = @{}
+    if ($computerName) { $param.computerName = $computerName }
+    if ($getDataFromIntune) { $param.getDataFromIntune = $true }
+    if ($credential) { $param.credential = $credential }
+    if ($tenantId) { $param.tenantId = $tenantId }
 
-        if ($tenantId) {
-            # app logon
-            if (!$credential) {
-                $credential = Get-Credential -Message "Enter AppID and AppSecret for connecting to Intune tenant" -ErrorAction Stop
-            }
-            Update-MSGraphEnvironment -AppId $credential.UserName -Quiet
-            Update-MSGraphEnvironment -AuthUrl "https://login.windows.net/$tenantId" -Quiet
-            $null = Connect-MSGraph -ClientSecret $credential.GetNetworkCredential().Password -ErrorAction Stop
-        } else {
-            # user logon
-            if ($credential) {
-                $null = Connect-MSGraph -Credential $credential -ErrorAction Stop
-                # $header = New-GraphAPIAuthHeader -credential $credential -ErrorAction Stop
-            } else {
-                $null = Connect-MSGraph -ErrorAction Stop
-                # $header = New-GraphAPIAuthHeader -ErrorAction Stop
-            }
-        }
+    Write-Verbose "Getting deployed Win32Apps"
+    $win32App = Get-IntuneWin32App @param
+    #endregion get deployed Win32Apps
 
-        Write-Verbose "Getting Intune data"
-        # filtering by ID is as slow as getting all data
-        # Invoke-MSGraphRequest -Url 'https://graph.microsoft.com/beta/deviceAppManagement/mobileApps?$filter=(id%20eq%20%2756695a77-925a-4df0-be79-24ed039afa86%27)'
-        $intuneApp = Invoke-MSGraphRequest -Url "https://graph.microsoft.com/beta/deviceAppManagement/mobileApps?select=id,displayname" | Get-MSGraphAllPages
-        $intuneUser = Invoke-MSGraphRequest -Url 'https://graph.microsoft.com/beta/users?select=id,userPrincipalName' | Get-MSGraphAllPages
-    }
-
-    if ($computerName) {
-        $session = New-PSSession -ComputerName $computerName -ErrorAction Stop
-    } else {
-        if (! ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-            throw "Run as administrator"
-        }
-    }
-    #endregion prepare
-
-    #region get data
-    $scriptBlock = {
-        param($verbosePref, $excludeSystemApp, $getDataFromIntune, $intuneApp, $intuneUser, $allFunctionDefs)
-
-        # inherit verbose settings from host session
-        $VerbosePreference = $verbosePref
-
-        # recreate functions from their text definitions
-        . ([ScriptBlock]::Create($allFunctionDefs))
-
-        foreach ($app in (Get-ChildItem "HKLM:\SOFTWARE\Microsoft\IntuneManagementExtension\Win32Apps" -ErrorAction SilentlyContinue)) {
-            $userAzureObjectID = Split-Path $app.Name -Leaf
-
-            if ($excludeSystemApp -and $userAzureObjectID -eq "00000000-0000-0000-0000-000000000000") {
-                Write-Verbose "Skipping system deployments"
-                continue
-            }
-
-            $userWin32AppRoot = $app.PSPath
-            $win32AppIDList = Get-ChildItem $userWin32AppRoot | select -ExpandProperty PSChildName | % { $_ -replace "_\d+$" } | select -Unique | ? { $_ -ne 'GRS' }
-
-            $win32AppIDList | % {
-                $win32AppID = $_
-
-                Write-Verbose "Processing App ID $win32AppID"
-
-                $newestWin32AppRecord = Get-ChildItem $userWin32AppRoot | ? PSChildName -Match ([regex]::escape($win32AppID)) | Sort-Object -Descending -Property PSChildName | select -First 1
-
-                $lastUpdatedTimeUtc = Get-ItemPropertyValue $newestWin32AppRecord.PSPath -Name LastUpdatedTimeUtc -ErrorAction SilentlyContinue
-                try {
-                    $complianceStateMessage = Get-ItemPropertyValue "$($newestWin32AppRecord.PSPath)\ComplianceStateMessage" -Name ComplianceStateMessage -ErrorAction Stop | ConvertFrom-Json -ErrorAction Stop
-                } catch {
-                    Write-Verbose "`tUnable to get Compliance State Message data"
-                }
-                try {
-                    $enforcementStateMessage = Get-ItemPropertyValue "$($newestWin32AppRecord.PSPath)\EnforcementStateMessage" -Name EnforcementStateMessage -ErrorAction Stop | ConvertFrom-Json -ErrorAction Stop
-                } catch {
-                    Write-Verbose "`tUnable to get Enforcement State Message data"
-                }
-
-                $lastError = $complianceStateMessage.ErrorCode
-                if (!$lastError) { $lastError = 0 } # because of HTML conditional formatting ($null means that cell will have red background)
-
-                if ($getDataFromIntune) {
-                    $property = [ordered]@{
-                        "Scope"              = _getTargetName $userAzureObjectID
-                        "DisplayName"        = ($intuneApp | ? id -EQ $win32AppID).DisplayName
-                        "Id"                 = $win32AppID
-                        "LastUpdatedTimeUtc" = $lastUpdatedTimeUtc
-                        # "Status"            = $complianceStateMessage.ComplianceState
-                        "ProductVersion"     = $complianceStateMessage.ProductVersion
-                        "LastError"          = $lastError
-                        "ScopeId"            = $userAzureObjectID
-                    }
-                } else {
-                    $property = [ordered]@{
-                        "Scope"              = _getTargetName $userAzureObjectID
-                        "DisplayName"        = Get-IntuneWin32AppName $win32AppID
-                        "Id"                 = $win32AppID
-                        "LastUpdatedTimeUtc" = $lastUpdatedTimeUtc
-                        # "Status"            = $complianceStateMessage.ComplianceState
-                        "ProductVersion"     = $complianceStateMessage.ProductVersion
-                        "LastError"          = $lastError
-                        "ScopeId"            = $userAzureObjectID
-                    }
-                }
-
-                New-Object -TypeName PSObject -Property $property
-            }
-        }
-    }
-
-    $param = @{
-        scriptBlock  = $scriptBlock
-        argumentList = ($VerbosePreference, $excludeSystemApp, $getDataFromIntune, $intuneApp, $intuneUser, $allFunctionDefs)
-    }
-    if ($computerName) {
-        $param.session = $session
-    }
-
-    $win32App = Invoke-Command @param | select -Property * -ExcludeProperty PSComputerName, RunspaceId, PSShowComputerName
-    #endregion get data
-
-    #region let user redeploy chosen app
     if ($win32App) {
         $hasDisplayNameProp = $win32App | Get-Member -Name DisplayName
         $appToRedeploy = $win32App | ? { if ($hasDisplayNameProp) { if ($_.DisplayName) { $true } } else { $true } } | Out-GridView -PassThru -Title "Pick app(s) for redeploy"
 
+        #region redeploy selected Win32Apps
         if ($appToRedeploy) {
             $scriptBlock = {
                 param ($verbosePref, $allFunctionDefs, $appToRedeploy)
@@ -337,6 +119,8 @@
                     $appId = $_.id
                     $scopeId = $_.scopeId
                     if ($scopeId -eq 'device') { $scopeId = "00000000-0000-0000-0000-000000000000" }
+                    if (!$appId) { throw "ID property is missing. Problem is probably in function Get-IntuneWin32App." }
+                    if (!$scopeId) { throw "ScopeId property is missing. Problem is probably in function Get-IntuneWin32App." }
                     Write-Warning "Preparing redeploy for app $appId (scope $scopeId)"
 
                     $win32AppKeyToDelete = $win32AppKeys | ? { $_.PSChildName -Match "^$appId`_\d+" -and $_.PSParentPath -Match "\\$scopeId$" }
@@ -362,7 +146,7 @@
                     }
                 }
 
-                Write-Warning "Invoking redeploy (by restarting service IntuneManagementExtension). Redeploy can take several minutes!"
+                Write-Warning "Invoking redeploy (by removing registry key and restarting service IntuneManagementExtension). Redeploy can take several minutes!"
                 Restart-Service IntuneManagementExtension -Force
             }
 
@@ -371,17 +155,13 @@
                 argumentList = ($VerbosePreference, $allFunctionDefs, $appToRedeploy)
             }
             if ($computerName) {
-                $param.session = $session
+                $param.computerName = $computerName
             }
 
             Invoke-Command @param
         }
+        #endregion redeploy selected Win32Apps
     } else {
         Write-Warning "No deployed Win32App detected"
-    }
-    #endregion let user redeploy chosen app
-
-    if ($computerName) {
-        Remove-PSSession $session
     }
 }

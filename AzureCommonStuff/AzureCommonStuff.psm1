97740b38040f13444f9e01b8ca22e9a96b6ac2e6
ztrhgf <ztrhgf@seznam.cz>
2024-03-11 10:46:45 +0100

    AzureCommonStuff 1.0.1

--- AzureCommonStuff/AzureCommonStuff.psm1
+++ AzureCommonStuff/AzureCommonStuff.psm1
@@ -5,18 +5,25 @@
 
     .DESCRIPTION
     Function for connecting to Azure using Connect-AzAccount command (Az.Accounts module).
-    In case there is already existing connection, stop.
+    In case there is already existing valid connection, no new will be created.
 
     .PARAMETER credential
     Credentials (User or App) for connecting to Azure.
     For App credentials tenantId must be set too!
+
+    .PARAMETER applicationId
+    ID of the service principal that will be used for connection.
+
+    .PARAMETER certificateThumbprint
+    Thumbprint of the locally stored certificate that will be used for connection.
+    Certificate has to be placed in personal machine store and user running this function has to have permission to read its private key.
 
     .PARAMETER servicePrincipal
     Switch for using App/Service Principal authentication instead of User auth.
 
     .PARAMETER tenantId
     Azure tenant ID.
-    Mandatory when App authentication is used .
+    Mandatory when App authentication is used.
 
     .EXAMPLE
     Connect-AzAccount2
@@ -35,6 +42,12 @@
 
     Authenticate to Azure using given app credentials (service principal).
 
+    .EXAMPLE
+    $thumbprint = Get-ChildItem Cert:\LocalMachine\My | ? subject -EQ "CN=contoso.onmicrosoft.com" | select -ExpandProperty Thumbprint
+    $null = Connect-AzAccount2 -ApplicationId 'cd2ae428-35f9-41b4-a527-71f2f8f1e5cf' -CertificateThumbprint $thumbprint -ServicePrincipal
+
+    Authenticate using certificate.
+
     .NOTES
     Requires module Az.Accounts.
     #>
@@ -43,13 +56,49 @@
     param (
         [System.Management.Automation.PSCredential] $credential,
 
+        [string] $applicationId,
+
+        [string] $certificateThumbprint,
+
         [switch] $servicePrincipal,
 
         [string] $tenantId = $_tenantId
     )
 
-    if (Get-AzContext) {
-        Write-Verbose "Already connected to Azure"
+    #region checks
+    $azAccesstoken = Get-AzAccessToken -ErrorAction SilentlyContinue
+
+    #region check whether there is valid existing session
+    $tenantIsDomainName = $false
+    $correctTenant = $false
+
+    if ($azAccesstoken -and $azAccesstoken.ExpiresOn -gt [datetime]::now) {
+        if ($tenantId -like "*.*") {
+            $tenantIsDomainName = $true
+        }
+        if (($tenantIsDomainName -and $azAccesstoken.UserId -like "*@$tenantId") -or (!$tenantIsDomainName -and $azAccesstoken.TenantId -eq $tenantId)) {
+            $correctTenant = $true
+        }
+    }
+    #endregion check whether there is valid existing session
+
+    #region check whether there is valid existing session created via required account
+    $userId = $null
+    $correctAccount = $false
+
+    if ($azAccesstoken -and ($applicationId -or $credential) -and ($azAccesstoken.UserId -eq $applicationId -or $azAccesstoken.UserId -eq $credential.UserName)) {
+        # there is an existing token that uses required account already
+        $correctAccount = $true
+    }
+    if ($azAccesstoken -and !$applicationId -and !$credential) {
+        # there is an existing token that can be used, because no explicit credentials were specified
+        $correctAccount = $true
+    }
+    #endregion check whether there is valid existing session created via required account
+    #endregion checks
+
+    if ($azAccesstoken -and $correctTenant -and $correctAccount) {
+        Write-Verbose "Already connected to the Azure using $($azAccesstoken.UserId)"
         return
     } else {
         if ($servicePrincipal -and !$tenantId) {
@@ -60,6 +109,8 @@
         if ($servicePrincipal) { $param.servicePrincipal = $true }
         if ($tenantId) { $param.tenantId = $tenantId }
         if ($credential) { $param.credential = $credential }
+        if ($applicationId) { $param.applicationId = $applicationId }
+        if ($certificateThumbprint) { $param.certificateThumbprint = $certificateThumbprint }
 
         Connect-AzAccount @param
     }
--- AzureCommonStuff_source/Connect-AzAccount2.ps1
+++ AzureCommonStuff_source/Connect-AzAccount2.ps1
@@ -6,18 +6,25 @@ function Connect-AzAccount2 {
 
     .DESCRIPTION
     Function for connecting to Azure using Connect-AzAccount command (Az.Accounts module).
-    In case there is already existing connection, stop.
+    In case there is already existing valid connection, no new will be created.
 
     .PARAMETER credential
     Credentials (User or App) for connecting to Azure.
     For App credentials tenantId must be set too!
+
+    .PARAMETER applicationId
+    ID of the service principal that will be used for connection.
+
+    .PARAMETER certificateThumbprint
+    Thumbprint of the locally stored certificate that will be used for connection.
+    Certificate has to be placed in personal machine store and user running this function has to have permission to read its private key.
 
     .PARAMETER servicePrincipal
     Switch for using App/Service Principal authentication instead of User auth.
 
     .PARAMETER tenantId
     Azure tenant ID.
-    Mandatory when App authentication is used .
+    Mandatory when App authentication is used.
 
     .EXAMPLE
     Connect-AzAccount2
@@ -36,6 +43,12 @@ function Connect-AzAccount2 {
 
     Authenticate to Azure using given app credentials (service principal).
 
+    .EXAMPLE
+    $thumbprint = Get-ChildItem Cert:\LocalMachine\My | ? subject -EQ "CN=contoso.onmicrosoft.com" | select -ExpandProperty Thumbprint
+    $null = Connect-AzAccount2 -ApplicationId 'cd2ae428-35f9-41b4-a527-71f2f8f1e5cf' -CertificateThumbprint $thumbprint -ServicePrincipal
+
+    Authenticate using certificate.
+
     .NOTES
     Requires module Az.Accounts.
     #>
@@ -44,13 +57,49 @@ function Connect-AzAccount2 {
     param (
         [System.Management.Automation.PSCredential] $credential,
 
+        [string] $applicationId,
+
+        [string] $certificateThumbprint,
+
         [switch] $servicePrincipal,
 
         [string] $tenantId = $_tenantId
     )
 
-    if (Get-AzContext) {
-        Write-Verbose "Already connected to Azure"
+    #region checks
+    $azAccesstoken = Get-AzAccessToken -ErrorAction SilentlyContinue
+
+    #region check whether there is valid existing session
+    $tenantIsDomainName = $false
+    $correctTenant = $false
+
+    if ($azAccesstoken -and $azAccesstoken.ExpiresOn -gt [datetime]::now) {
+        if ($tenantId -like "*.*") {
+            $tenantIsDomainName = $true
+        }
+        if (($tenantIsDomainName -and $azAccesstoken.UserId -like "*@$tenantId") -or (!$tenantIsDomainName -and $azAccesstoken.TenantId -eq $tenantId)) {
+            $correctTenant = $true
+        }
+    }
+    #endregion check whether there is valid existing session
+
+    #region check whether there is valid existing session created via required account
+    $userId = $null
+    $correctAccount = $false
+
+    if ($azAccesstoken -and ($applicationId -or $credential) -and ($azAccesstoken.UserId -eq $applicationId -or $azAccesstoken.UserId -eq $credential.UserName)) {
+        # there is an existing token that uses required account already
+        $correctAccount = $true
+    }
+    if ($azAccesstoken -and !$applicationId -and !$credential) {
+        # there is an existing token that can be used, because no explicit credentials were specified
+        $correctAccount = $true
+    }
+    #endregion check whether there is valid existing session created via required account
+    #endregion checks
+
+    if ($azAccesstoken -and $correctTenant -and $correctAccount) {
+        Write-Verbose "Already connected to the Azure using $($azAccesstoken.UserId)"
         return
     } else {
         if ($servicePrincipal -and !$tenantId) {
@@ -61,6 +110,8 @@ function Connect-AzAccount2 {
         if ($servicePrincipal) { $param.servicePrincipal = $true }
         if ($tenantId) { $param.tenantId = $tenantId }
         if ($credential) { $param.credential = $credential }
+        if ($applicationId) { $param.applicationId = $applicationId }
+        if ($certificateThumbprint) { $param.certificateThumbprint = $certificateThumbprint }
 
         Connect-AzAccount @param
     }

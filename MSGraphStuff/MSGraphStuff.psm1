97557366cff829810a2050be0c4d5c0db07ea810
ztrhgf <ztrhgf@seznam.cz>
2025-07-24 13:31:05 +0200

    MSGraphStuff 1.1.11

--- MSGraphStuff/MSGraphStuff.psm1
+++ MSGraphStuff/MSGraphStuff.psm1
@@ -14,32 +14,18 @@ function Expand-MgAdditionalProperties {
     Get-MgGroupMember -GroupId 90daa3a7-7fed-4fa7-a979-db74bcd7cbd0  | Expand-MgAdditionalProperties
 
     .EXAMPLE
-    Get-MgDirectoryObjectById -ids 34568a12-8862-45cf-afef-9582cd9871c6 | Expand-MgAdditionalProperties
+    Get-MgDirectoryObjectById -ids 34568a12-8861-45ff-afef-9282cd9871c6 | Expand-MgAdditionalProperties
     #>
 
     [CmdletBinding()]
     param(
         [parameter(ValueFromPipeline)]
-        [object] $inputObject
+        [object[]] $inputObject
     )
 
     process {
-        if ($inputObject.AdditionalProperties -and $inputObject.AdditionalProperties.gettype().name -eq 'Dictionary`2') {
-            $inputObject.AdditionalProperties.GetEnumerator() | % {
-                $item = $_
-                Write-Verbose "Adding property '$($item.key)' to the pipeline object"
-                $inputObject | Add-Member -MemberType NoteProperty -Name $item.key -Value $item.value
-
-                if ($item.key -eq "@odata.type") {
-                    Write-Verbose "Adding extra property 'ObjectType' to the pipeline object"
-                    $inputObject | Add-Member -MemberType NoteProperty -Name 'ObjectType' -Value ($item.value -replace [regex]::Escape("#microsoft.graph."))
-                }
-            }
-
-            $inputObject | Select-Object -Property * -ExcludeProperty AdditionalProperties
-        } else {
-            Write-Verbose "There is no 'AdditionalProperties' property"
-            $inputObject
+        foreach ($object in $inputObject) {
+            $object | Expand-ObjectProperty -Property AdditionalProperties -addObjectType
         }
     }
 }
